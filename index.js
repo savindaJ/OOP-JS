c374edb99a99f1f668b8a0fd43129b19befd7cbf
SavindaJayasekara <thantrige32@gmail.com>
2023-11-02 09:30:01 +0530

    add litaral base obj

---
+++ index.js
@@ -0,0 +1,16 @@
+// object literal syntax
+const person = {
+    name:"kamal", //property's
+    age:20,//property's
+    address:"matara",//property's
+    salary:120000,//property's
+    hobby:{//property's
+        hob1:"footBall",
+        hob2:"cricket"
+    },
+    eat:function (){ //function
+        console.log("Person eat")
+    }
+};
+
+person.eat();
