55103cdb45f90d5350ac1d438f4d93ea4d109f51
SavindaJayasekara <thantrige32@gmail.com>
2023-11-02 11:09:26 +0530

    add inheritance !

--- inheritance.js
+++ inheritance.js
@@ -10,7 +10,23 @@ class Animals {
         return `${this.name} can dance`;
     }
 }
+
+class Cat extends Animals{
+    constructor(name, age, whiskerColor) {
+        super(name, age);
+        this.whiskerColor = whiskerColor;
+    }
+    whiskers() {
+        return `I have ${this.whiskerColor} whiskers`;
+    }
+}
 let bingo = new Animals("Bingo", "Hairy");
 console.log(bingo);
 console.log(bingo.sing());
 console.log(bingo.dance());
+
+const cat = new Cat("Garfieald",4,"brown and white");
+console.log(cat.whiskers());
+console.log(cat.sing());
+console.log(cat.dance());
+
